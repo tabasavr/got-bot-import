d00b5cbd687eabcf5c41ef1a17d23b12a48b7d61
Sergei Kozelko <KozelkoS@yandex.ru>
2024-01-30 13:02:02 +0000

    Run prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,47 @@
 <script lang="ts">
-    let formattedCommands: string[] = []
-    let dataInput: string = ""
+	let formattedCommands: string[] = [];
+	let dataInput: string = '';
 
-    // escape `'` in the name because bot doesn't support them
-    function escapeName(name: string): string {
-        function escapeSegment(s: string): string {
-            if (s.startsWith("'") && s.endsWith("'")) {
-                return escapeSegment(s.substring(1, s.length - 1))
-            }
-            const index = s.indexOf("'")
-            if (index == -1) {
-                return s
-            } else {
-                return s.substring(index + 1)
-            }
-        }
-        return name.split(" ").map(escapeSegment).join(" ")
-    }
+	// escape `'` in the name because bot doesn't support them
+	function escapeName(name: string): string {
+		function escapeSegment(s: string): string {
+			if (s.startsWith("'") && s.endsWith("'")) {
+				return escapeSegment(s.substring(1, s.length - 1));
+			}
+			const index = s.indexOf("'");
+			if (index == -1) {
+				return s;
+			} else {
+				return s.substring(index + 1);
+			}
+		}
+		return name.split(' ').map(escapeSegment).join(' ');
+	}
 
-    function formatCrew(_event: Event) {
-        const data = JSON.parse(dataInput)
-        const crew: any[] = data["player"]["character"]["crew"]
-        formattedCommands = crew
-            .filter((member) => !member["in_buy_back_state"])
-            .map((member) => {
-                const name = escapeName(member["name"])
-                const stars = member["rarity"]
-                const level = member["level"]
-                return `-got bot crew add ${name} -s${stars} -l${level}`
-            })
-        dataInput = ""
-    }
+	function formatCrew(_event: Event) {
+		const data = JSON.parse(dataInput);
+		const crew: any[] = data['player']['character']['crew'];
+		formattedCommands = crew
+			.filter((member) => !member['in_buy_back_state'])
+			.map((member) => {
+				const name = escapeName(member['name']);
+				const stars = member['rarity'];
+				const level = member['level'];
+				return `-got bot crew add ${name} -s${stars} -l${level}`;
+			});
+		dataInput = '';
+	}
 
-    function copyToClipboard(text: string) {
-        navigator.clipboard.writeText(text)
-    }
+	function copyToClipboard(text: string) {
+		navigator.clipboard.writeText(text);
+	}
 </script>
 
 <h1>GoT bot crew import formatter</h1>
 
-<input type="text" bind:value={dataInput}/>
-<input type="button" on:click={formatCrew} value="Format"/>
+<input type="text" bind:value={dataInput} />
+<input type="button" on:click={formatCrew} value="Format" />
 
 {#each formattedCommands as command (command)}
-    <p><button on:click={() => copyToClipboard(command)}>Copy</button> {command}</p>
+	<p><button on:click={() => copyToClipboard(command)}>Copy</button> {command}</p>
 {/each}
--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
+		}
 	}
 };
 
