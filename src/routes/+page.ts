fcdaa8836b47f9ca73f5915e2de8e49657722abb
Sergei Kozelko <KozelkoS@yandex.ru>
2024-03-23 11:11:37 +0000

    Fix json loading error

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,7 @@
 export const ssr = false; //todo: can this be set to true?
 
 export async function load({ params }) {
-	let archetypes = await fetch('/crew.json'); 
+	let archetypes = await fetch('/got-bot-import/crew.json'); 
     return {
         "archetypes": await archetypes.json()
     };
